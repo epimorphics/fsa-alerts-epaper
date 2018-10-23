e90fdc898024c38c2ce0c9ef8ec7f8c7bab9d860
Max Prettyjohns <heshoots9999@gmail.com>
2018-10-23 13:40:06 +0100

    Add time modified and fsa name

--- main.py
+++ main.py
@@ -8,39 +8,44 @@ import time
 
 COLORED = 0
 UNCOLORED = 255 
-HASHTAG_X = 142
-HASHTAG_Y = 100
-ALERT_TYPE_X = 200
+HASHTAG_X = 10
+HASHTAG_Y = 145 
+ALERT_TYPE_X = 15
 ALERT_TYPE_Y = 20
 SLEEP_TIME = 60 * 15 #15 minutes
 title_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 40)
-alert_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 20)
+fsa_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 30)
+alert_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 20) 
 
 def draw_background(draw_black, draw_red):
     draw_red.rectangle((0, 0, 400, 140), fill = COLORED)
     epdHelpers.draw_shaded_rectange(draw_red, draw_black, 0, 140, 400, 300)
     draw_red.rectangle((10, 170, 390, 280), fill = COLORED)
     draw_black.rectangle((10, 170, 390, 280), fill = UNCOLORED)
+    draw_red.text((210, ALERT_TYPE_Y), "Food\nStandards\nAgency", font=fsa_font, fill=UNCOLORED, align="right")
+    draw_black.text((210, ALERT_TYPE_Y), "Food\nStandards\nAgency", font=fsa_font, fill=UNCOLORED, align="right")
 
 def draw_alert_type(draw_black, draw_red, alert):
     alerttype = fsa.get_alert_type(alert)
     if alerttype == fsa.AlertType.PRIN:
-        draw_red.text((ALERT_TYPE_X, ALERT_TYPE_Y), 'Product\nrecall', font = title_font, fill = UNCOLORED)
-        draw_red.text((HASHTAG_Y, HASHTAG_X), '#FoodAlert', font = alert_font, fill = UNCOLORED)
-        draw_black.text((HASHTAG_Y, HASHTAG_X), '#FoodAlert', font = alert_font, fill = UNCOLORED)
+        draw_red.multiline_text((ALERT_TYPE_X, ALERT_TYPE_Y), 'Product\nRecall', font = title_font, fill = UNCOLORED, align="left")
+	draw_red.text((HASHTAG_X, HASHTAG_Y), '#FoodAlert', font = fsa_font, fill = UNCOLORED)
+        draw_black.text((HASHTAG_X, HASHTAG_Y), '#FoodAlert', font = fsa_font, fill = UNCOLORED)
     elif alerttype == fsa.AlertType.AA:
-        draw_red.text((ALERT_TYPE_X, ALERT_TYPE_Y), 'Allergy\nalert', font = title_font, fill = UNCOLORED)
-        draw_red.text((HASHTAG_Y, HASHTAG_X), '#FoodAllergy', font = alert_font, fill = UNCOLORED)
-        draw_black.text((HASHTAG_Y, HASHTAG_X), '#FoodAllergy', font = alert_font, fill = UNCOLORED)
+        draw_red.multiline_text((ALERT_TYPE_X, ALERT_TYPE_Y), 'Allergy\nAlert', font = fsa_font, fill = UNCOLORED, align="left")
+        draw_red.text((HASHTAG_X, HASHTAG_Y), '#FoodAllergy', font = alert_font, fill = UNCOLORED)
+        draw_black.text((HASHTAG_X, HASHTAG_Y), '#FoodAllergy', font = alert_font, fill = UNCOLORED)
     else:
-        draw_red.text((ALERT_TYPE_X, ALERT_TYPE_Y), 'Alert', font = title_font, fill = UNCOLORED)
-        draw_red.text((HASHTAG_Y, HASHTAG_X), '#FoodAlert', font = alert_font, fill = UNCOLORED)
-        draw_black.text((HASHTAG_Y, HASHTAG_X), '#FoodAlert', font = alert_font, fill = UNCOLORED)
+        draw_red.multiline_text((ALERT_TYPE_X, ALERT_TYPE_Y), 'Alert', font = fsa_font, fill = UNCOLORED, align="left")
+        draw_red.text((HASHTAG_X, HASHTAG_Y), '#FoodAlert', font = alert_font, fill = UNCOLORED)
+        draw_black.text((HASHTAG_X, HASHTAG_Y), '#FoodAlert', font = alert_font, fill = UNCOLORED)
 
 def draw_text_area(draw_black, draw_red, alert):
-    text = epdHelpers.fill_draw_area(alert_font, "latest alert: " + alert['shortURL'] + " " + alert['title'], 30, 370, 170, 280)
-    draw_black.text((30, 170), text, font = alert_font, fill = UNCOLORED)
-    draw_red.text((30, 170), text, font = alert_font, fill = UNCOLORED)
+    text = epdHelpers.fill_draw_area(alert_font, "latest alert: " + alert['shortURL'] + " " + alert['title'], 30, 370, 175, 280)
+    draw_black.text((170, HASHTAG_Y), alert['modified'][0:19], font = alert_font, fill = UNCOLORED)
+    draw_red.text((170, HASHTAG_Y), alert['modified'][0:19], font = alert_font, fill = UNCOLORED)
+    draw_black.text((30, 175), text, font = alert_font, fill = UNCOLORED)
+    draw_red.text((30, 175), text, font = alert_font, fill = UNCOLORED)
 
 def main():
     oldalert = {'@id': ''}
